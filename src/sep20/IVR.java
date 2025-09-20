767f64d7543dfb25cf9fdd871599e0d59e84388b
pragra <sunny@pragra.io>
2025-09-20 11:06:09 -0400

    IVR

---
+++ src/sep20/IVR.java
@@ -0,0 +1,17 @@
+package sep20;
+
+import java.util.Scanner;
+
+public class IVR {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String choice = scanner.next();
+
+        if(choice == "English"){
+            // press
+
+        }else {
+
+        }
+    }
+}
